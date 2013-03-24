fee4117612283513fc9999ba4cdc0283b99dc48c
luxerama <vincent@siebert.im>
2013-03-24 20:32:43 +0000

    updating to solr v4.2.0

--- files/brews/solr.rb
+++ files/brews/solr.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.1.0/solr-4.1.0.tgz'
-  sha1 '89cce8c7c8d68df20f009a7c04ede423d6fa7c84'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.2.0/solr-4.2.0.tgz'
+  sha1 '3525a424be3c7f5de27ea1ab60b267cc0670b6c4'
 
-  version '4.1.0-boxen1'
+  version '4.2.0-boxen1'
 
   def script; <<-EOS.undent
     #!/bin/sh
