0ffc84ffdaa7978b7518f93ac46535e1c03cfbe8
luxerama <vincent@siebert.im>
2013-04-18 18:02:40 +0100

    adding solr puppet development

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,7 @@ class solr::config {
 
   $solrhome    = "${boxen::config::homebrewdir}/opt/solr/libexec/example"
   $configdir   = "${solrhome}/multicore"
+  $solrconfig  = "${configdir}/solr.xml"
   $datadir     = "${boxen::config::datadir}/solr"
   $logdir      = "${boxen::config::logdir}/solr"
   $executable  = "${solrhome}/start.jar"
